43f577750f4965423fa44a79086cb63de0ea67d2
pouyasadri <pouyapaghandeh78@gmail.com>
2026-04-01 03:13:01 +0200

    add server implementation for TCP chat application

--- cmd/chat-server/main.go
+++ cmd/chat-server/main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"log"
 	"net"
+
+	"github.com/pouyasadri/go-tcp-chat/internal/chat"
 )
 
 func main() {
-
-	s := NewServer()
+	s := chat.NewServer()
 	go s.Run()
 
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatal("Unable to start server: ", err.Error())
+		log.Fatal("unable to start server: ", err)
 	}
 	defer listener.Close()
-	log.Printf("Server started on :8080")
 
+	log.Printf("server started on :8080")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Failed to accept connection: %s", err.Error())
+			log.Printf("failed to accept connection: %v", err)
 			continue
 		}
 		go s.NewClient(conn)
